e3a65f8e18ef702e2c01bd0e05794497d370921f
haggishunk <travis@mattera.io>
2021-03-01 20:40:52 -0800

    Changed spacefish symbols and order

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,16 @@
 fish_vi_key_bindings
 
 # spacefish
-set -gx SPACEFISH_PROMPT_ORDER time user dir host git venv package node docker golang rust aws kubecontext line_sep battery char
+set -gx SPACEFISH_PROMPT_ORDER time user dir host git venv package rust node docker golang aws kubecontext line_sep battery char
 set -gx SPACEFISH_GIT_SYMBOL ' '
 set -gx SPACEFISH_VENV_SYMBOL ' '
 set -gx SPACEFISH_VENV_SHOW true
 set -gx SPACEFISH_VENV_GENERIC_NAMES .virtualenvs
 set -gx SPACEFISH_KUBECONTEXT_SYMBOL 'ﴱ '
 set -gx SPACEFISH_VI_MODE_SHOW false
-set -gx SPACEFISH_GOLANG_SYMBOL 
+set -gx SPACEFISH_PACKAGE_SYMBOL ' '
+set -gx SPACEFISH_GOLANG_SYMBOL ' '
+set -gx SPACEFISH_RUST_SYMBOL ' '
 set -gx SPACEFISH_CHAR_SYMBOL 
 set -gx SPACEFISH_AWS_SYMBOL ' '
 
