72b04195889028f6866960011542dd83f5cae0f2
Travis Mattera <travis@credo.ai>
2024-08-12 09:25:32 -0700

    Changed python lsp settings.

--- config/nvim/lua/lsp.lua
+++ config/nvim/lua/lsp.lua
@@ -180,13 +180,13 @@ lspconfig.pylsp.setup{
           maxLineLength = 100
         },
         pydocstyle = {
-          enabled = true,
+          enabled = false,
         },
         pyflakes = {
-          enabled = true,
+          enabled = false,
         },
         pylint = {
-          enabled = true,
+          enabled = false,
         },
         pylsp_black = {
           enabled = false,
@@ -219,7 +219,7 @@ lspconfig.pylsp.setup{
 
 local function on_attach_pyright(client, bufnr)
     set_omnifunc(client, bufnr)
-    lsp_org_imports_on_save(client, bufnr)
+    -- lsp_org_imports_on_save(client, bufnr)
     -- Call additional setup functions here
 end
 -- https://packagecontrol.io/packages/LSP-pyright
