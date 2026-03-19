9006db67387c171807848722aa23e6af8fc386cd
Travis Mattera <travis@mattera.io>
2026-03-19 14:28:31 -0700

    chore: fix treesitter config

--- config/nvim/lua/config/treesitter.lua
+++ config/nvim/lua/config/treesitter.lua
@@ -3,7 +3,7 @@
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_perl_provider = 0
 
-require'nvim-treesitter.configs'.setup {
+require'nvim-treesitter.config'.setup {
   ensure_installed = { "c", "lua", "rust", "yaml", "vim", "python", "dockerfile", "go", "mermaid", "markdown", "terraform", "hcl", "typescript", "toml", "css", "html", "javascript", "norg", "regex", "scss", "svelte", "tsx", "typst", "vue"},
   auto_install = false,
   highlight = {
